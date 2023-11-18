9ad95a8926116d07dc2168f01e1adb5d76be47ad
teddyvicker <vicpoliveira@gmail.com>
2023-11-18 15:05:33 -0400

    Exercicio 02

---
+++ Exercicio_02/ListaNumero.cs
@@ -0,0 +1,34 @@
+﻿
+namespace Exercicio_02
+{
+    public class ListaNumeros
+    {
+        public List<int> Numeros { get; private set; }
+
+        public ListaNumeros()
+        {
+            Numeros = new List<int>();
+        }
+
+        public void AdicionarNumero(int numero)
+        {
+            Numeros.Add(numero);
+        }
+    }
+
+    public static class ListaNumerosExtensions
+    {
+        public static int CalcularSoma(this ListaNumeros lista)
+        {
+            return lista.Numeros.Sum();
+        }
+
+        public static double CalcularMedia(this ListaNumeros lista)
+        {
+            if (lista.Numeros.Count == 0)
+                return 0;
+
+            return lista.Numeros.Average();
+        }
+    }
+}
---
+++ Exercicio_02/Program.cs
@@ -0,0 +1,20 @@
+﻿using Exercicio_02;
+
+class Program
+{
+    static void Main()
+    {
+        ListaNumeros minhaLista = new ListaNumeros();
+
+        minhaLista.AdicionarNumero(10);
+        minhaLista.AdicionarNumero(20);
+        minhaLista.AdicionarNumero(30);
+
+        //soma e a média
+        int soma = minhaLista.CalcularSoma();
+        double media = minhaLista.CalcularMedia();
+
+        Console.WriteLine($"Soma: {soma}");
+        Console.WriteLine($"Média: {media}");
+    }
+}
